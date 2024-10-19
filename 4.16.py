106e0525a2f51bc78618d8c1ed6fb29774e9cdfa
jsullivan54 <jsullivan54@student.monroecc.edu>
2024-10-19 19:02:29 -0400

    Update 4.16.py

--- 4.16.py
+++ 4.16.py
@@ -14,7 +14,7 @@ while another == "y" or another =="Y":
                             "wholesale cost:"))
 
     # Calculate the retail price.
-    retail = wholesale * MARK_UP
+    retail = wholesale * mark_up
 
     # Display the retail price.
     print(f'Retail price:${retail:,.2f}')
